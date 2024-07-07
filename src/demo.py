90d1af31d25468c7cc9b7f784a1cc0d30110181b
leandermaerkisch <leander.maerkisch@floy.com>
2024-07-06 23:07:04 -0600

    Remove exa deps

--- src/demo.py
+++ src/demo.py
@@ -1,84 +1,12 @@
 import gradio as gr
 from PIL import Image
-import requests
-import json
 import os
-from io import BytesIO
-import base64
 from pinecone import Pinecone
 from rag import get_single_image_embedding, get_image
 
 from dotenv import load_dotenv
 
 load_dotenv()
-
-from exa_py import Exa
-
-exa = Exa(api_key=os.getenv("EXA_API_KEY"))
-
-
-def search(query: str): 
-    result = exa.search_and_contents(
-        query=query, 
-        type="neural",
-        num_results=5,
-        text=True,
-        category="research paper"
-    )
-
-    return result
-
-
-url = "https://api.fireworks.ai/inference/v1/chat/completions"
-payload = {
-  "model": "accounts/fireworks/models/llava-yi-34b",
-  "max_tokens": 512,
-  "top_p": 1,
-  "top_k": 40,
-  "presence_penalty": 0,
-  "frequency_penalty": 0,
-  "temperature": 0.6,
-  "messages": []
-}
-headers = {
-  "Accept": "application/json",
-  "Content-Type": "application/json",
-  "Authorization": f"Bearer {os.getenv("EXA_API_KEY")}"
-}    
-def generate_description(image):
-    buffered = BytesIO()
-    image.save(buffered, format="JPEG")
-    image_data = base64.b64encode(buffered.getvalue()).decode("utf-8")
-
-    payload = {
-        "model": "accounts/fireworks/models/llava-yi-34b",
-        "max_tokens": 512,
-        "top_p": 1,
-        "top_k": 40,
-        "presence_penalty": 0,
-        "frequency_penalty": 0,
-        "temperature": 0.6,
-        "messages": [
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": "Describe the content of this image in detail. Be succinct"
-                    },
-                    {
-                        "type": "image",
-                        "image": image_data
-                    }
-                ]
-            }
-        ]
-    }
-
-
-    response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
-    return response.json()
-
 
 def find_similar_images(image):
     pinecone = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
